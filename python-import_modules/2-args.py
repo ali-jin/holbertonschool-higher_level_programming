fcd7a6365311955c3d026030eb9f2081f175944e
root <root@pc>
2023-01-18 17:54:36 +0100

    fix space in print

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(1, (len(sys.argv))):
-            print("{}: ".format(i),  sys.argv[i])
+            print("{}:".format(i),  sys.argv[i])
     else:
         print("{} arguments.".format(len(sys.argv) - 1))
