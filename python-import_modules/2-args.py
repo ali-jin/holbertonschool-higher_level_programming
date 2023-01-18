8e6f43daa93c379ff6d8e76d8ac6ebeb7d1f50db
root <root@pc>
2023-01-18 18:00:25 +0100

    add elif condition

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,9 +2,12 @@
 if __name__ == "__main__":
     import sys
 
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
         print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(1, (len(sys.argv))):
             print("{}:".format(i),  sys.argv[i])
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
+        print("{}:".format(1),  sys.argv[1])
     else:
-        print("{} arguments.".format(len(sys.argv) - 1))
+        print("{} argument.".format(len(sys.argv) - 1))
