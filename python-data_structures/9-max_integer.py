f2837d478c53efb9d4d1e51126960cd301fc61f8
root <root@pc>
2023-01-23 14:08:44 +0100

    fix if condition and for loop

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,10 +3,10 @@
 
 def max_integer(my_list=[]):
 
-    maxNum = 0
-    if my_list is None:
+    maxNum = my_list[0]
+    if my_list == []:
         return None
-    for i in range(len(my_list)):
+    for i in range(1, len(my_list)):
         if my_list[i] > maxNum:
             maxNum = my_list[i]
     return maxNum
