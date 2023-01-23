005fdc61ddf2758bc9d44aff95a5c5a73f6f07a8
root <root@pc>
2023-01-23 12:51:30 +0100

    add another if condition

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -11,3 +11,5 @@ def print_matrix_integer(matrix=[[]]):
                     print(" ", end="")
                 if row.index(List) == len(row) - 1:
                     print("")
+    else:
+        print("")
