f1fb3a70510193f433f90b19910e4bb89a8bafe3
root <root@pc>
2023-01-23 12:46:47 +0100

    function that prints a matrix of integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,7 +6,7 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is not None:
         for row in matrix:
             for List in row:
-                print("{}".format(List), end='')
+                print("{:d}".format(List), end='')
                 print(" ", end="")
                 if row.index(List) == len(row) - 1:
                     print("")
