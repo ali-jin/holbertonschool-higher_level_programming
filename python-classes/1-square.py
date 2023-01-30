e289b932a555c79ebdafbaff1f33ffa1b0266cc2
root <root@pc>
2023-01-30 14:28:57 +0100

    add private instance attribute 'size'

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A Class Square"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, __size=0):
+        """Initialize the size of the square
+
+        Args:
+            __size (int): Size of the square.
+        """
+        self.__size = __size
