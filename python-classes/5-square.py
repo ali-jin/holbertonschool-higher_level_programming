e42b94a114d64222165fa5d63eb747e9591a1f5b
root <root@pc>
2023-01-30 15:33:10 +0100

    add function def my_print(self) that print the square

---
+++ python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""A Class Square"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0):
+        """Initialize the size of the square
+
+        Args:
+            __size (int): Size of the square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """int: the area of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Property of square"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate the area square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square"""
+        for row in range(self.__size):
+            for col in range(self.__size):
+                print("#", end="")
+            print("")
