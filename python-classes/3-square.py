1195dbfc212d60b539b6cc715f1798260382f3b6
root <root@pc>
2023-01-30 14:50:53 +0100

    add the function def area(self) that calculate the area of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A Class Square"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, __size=0):
+        """Initialize the size of the square
+
+        Args:
+            __size (int): Size of the square.
+        """
+        self.__size = __size
+        if type(__size) != int:
+            raise TypeError("size must be an integer")
+        if __size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Calculate the area square
+
+        Returns:
+            int : result of area square
+        """
+        return (self.__size) ** 2
