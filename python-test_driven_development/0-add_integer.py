75b903fa40acd481a7b468a173c0647f6d54b1ed
root <root@pc>
2023-01-31 17:49:42 +0100

    0-add_integer.py and 0-add_integer.txt

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def add_integer(a, b=98):
+    """"Return the function of a + b"""
+    if isinstance(a, (int, float)):
+        a = int(a)
+    else:
+        raise TypeError("a must be an integer")
+    if isinstance(b, (int, float)):
+        b = int(b)
+    else:
+        raise TypeError("b must be an integer")
+
+    return a + b
