9de3c122f917c49c551c6b2402874b01ba097da5
root <root@pc>
2023-02-02 14:29:03 +0100

    count the number of instances

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """A class that define a rectangle"""
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initialize the width and height of the rectangle
 
