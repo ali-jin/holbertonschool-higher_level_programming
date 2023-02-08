e56585b14c7eb7e81f47d98f2774cbb3ecbf1d97
root <root@pc>
2023-02-08 14:29:49 +0100

    add function area() and __str__

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that contains a class BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class Rectangle that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        width = self.__width
+        height = self.__height
+        return ''.join("[Rectangle] " + str(width) + "/" + str(height))
