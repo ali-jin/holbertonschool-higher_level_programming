45fb4866973f8eff6afc0029469207de6689f5fa
root <root@pc>
2023-02-08 16:45:27 +0100

    a function that returns the JSON representation of an object (string)

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -10,6 +10,5 @@ def to_json_string(my_obj):
     Args:
         my_obj (str): The string to transform
     """
-    json_string = json.dumps(my_obj)
-    print(json_string)
-    print(type(json_string))
+    return json.dumps(my_obj)
+
