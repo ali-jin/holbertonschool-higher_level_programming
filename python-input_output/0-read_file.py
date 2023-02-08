53c1e94dc3b9e0062605d9b365e8bc4587319765
root <root@pc>
2023-02-08 15:17:05 +0100

    function that reads a text file (UTF8) and prints it to stdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that read and print a text file"""
+
+
+def read_file(filename=""):
+    """function that reads a text file (UTF8)
+       and prints it to stdout
+
+    Args:
+        filename (str): A text file.
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
+        print()
