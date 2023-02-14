5c7581e7983c93133a542cabc3be6f3ee400831f
root <root@pc>
2023-02-14 15:30:01 +0100

    add the function to_json_string()

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module that contains class Base"""
+import json
 
 
 class Base:
@@ -13,3 +14,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation
+           of list_dictionaries
+        """
+        if list_dictionaries == [] or list_dictionaries is None:
+            return []
+        else:
+            return json.dumps(list_dictionaries)
