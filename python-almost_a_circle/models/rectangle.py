c821c69c50b431fae9ab743d85d017f354f0f344
root <root@pc>
2023-02-14 15:07:36 +0100

    add function to_dictionary

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -106,6 +106,8 @@ class Rectangle(Base):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle
+        """
         return {
             'id': self.id,
             'width': self.width,
