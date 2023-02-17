53235038c05c06875747ca23d475fb16e2b09004
root <root@pc>
2023-02-17 19:33:12 +0100

    test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -6,21 +6,47 @@ from models.base import Base
 
 class test_base(unittest.TestCase):
 
-    def test_unique_id(self):
-        """Test that a new instance of Base has a unique ID"""
-        b1 = Base()
-        b2 = Base()
-        self.assertNotEqual(b1.id, b2.id, "ID should be unique")
+    def setUp(self):
+        """ Method invoked for each test """
+        Base._Base__nb_objects = 0
 
-    def test_automatic_id(self):
-        """Test that an ID is correctly assigned to a new instance of Base"""
-        b1 = Base()
-        b2 = Base()
-        self.assertEqual(b1.id, b2.id - 1)
+    def test_id(self):
+        """ Test assigned id """
+        new = Base(1)
+        self.assertEqual(new.id, 1)
 
-    def test_passed_id(self):
-        b1 = Base(89)
-        self.assertEqual(b1.id, 89)
+    def test_id_default(self):
+        """ Test default id """
+        new = Base()
+        self.assertEqual(new.id, 1)
+
+    def test_id_nb_objects(self):
+        """ Test nb object attribute """
+        new = Base()
+        new2 = Base()
+        new3 = Base()
+        self.assertEqual(new.id, 1)
+        self.assertEqual(new2.id, 2)
+        self.assertEqual(new3.id, 3)
+
+    def test_id_mix(self):
+        """ Test nb object attributes and assigned id """
+        new = Base()
+        new2 = Base(1024)
+        new3 = Base()
+        self.assertEqual(new.id, 1)
+        self.assertEqual(new2.id, 1024)
+        self.assertEqual(new3.id, 2)
+
+    def test_string_id(self):
+        """ Test string id """
+        new = Base('1')
+        self.assertEqual(new.id, '1')
+
+    def test_more_args_id(self):
+        """ Test passing more args to init method """
+        with self.assertRaises(TypeError):
+            new = Base(1, 1)
 
     def test_to_json_string(self):
         """Test to_json_string method with None argument"""
