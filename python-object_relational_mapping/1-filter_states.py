4e2cd16f14117f342dba7a8d7adc19ed3a83149a
root <root@pc>
2023-03-16 14:58:51 +0100

    add 'BINARY' in cursor.execute()

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE NAME LIKE 'N%';")
+    cursor.execute("SELECT * FROM states WHERE NAME LIKE BINARY 'N%';")
     result = cursor.fetchall()
 
     for row in result:
