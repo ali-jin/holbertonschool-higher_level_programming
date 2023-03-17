933085bf521aabd30dac1ecc6736bbf4494b5615
root <root@pc>
2023-03-17 16:05:42 +0100

    script that changes the name of a State object from the database hbtn_0e_6_usa

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script that adds the State object “Louisiana”
+to the database hbtn_0e_6_usa"""
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]
+    ))
+
+    Base.metadata.create_all(bind=engine)
+    session = Session(engine)
+
+    new_state = session.query(State).filter(State.id == 2).first()
+    new_state.name = 'New Mexico'
+    session.commit()
