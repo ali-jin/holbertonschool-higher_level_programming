968877a44441562cf5a388cf4bd883f7a7b1ff22
root <root@pc>
2023-03-17 16:09:29 +0100

    fix commnts module

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""script that adds the State object “Louisiana”
-to the database hbtn_0e_6_usa"""
+"""script that changes the name of a State object
+from the database hbtn_0e_6_usa"""
 from model_state import Base, State
 from sys import argv
 from sqlalchemy import create_engine
