3ed678882a4751fc43267ac01f27c3cbc6ee4faa
root <root@pc>
2023-04-09 16:04:21 +0200

    script that prints a square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const process = require('process');
+const args = process.argv;
+const size = args[2];
+if (args.length === 2 || isNaN(parseInt(args[2]))) {
+  console.log('Missing size');
+}
+for (let i = 0; i < size; i++) {
+  let row = '';
+  for (let j = 0; j < size; j++) {
+    row += 'X';
+  }
+  console.log(row + ' ');
+}
