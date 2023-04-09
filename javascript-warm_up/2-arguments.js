24f1a4df0c78f7e47bff1a1b6a9c45550348c709
root <root@pc>
2023-04-09 14:50:43 +0200

    add condition 'else id' for 'Argument found' condition

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,6 +3,8 @@ const process = require('process');
 const nbArg = process.argv;
 if (nbArg.length === 2) {
   console.log('No argument');
+} else if (nbArg.length === 3) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
