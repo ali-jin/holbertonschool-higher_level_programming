3442ec024ba823e5f2aa33ce3817b78c0c713b35
root <root@pc>
2023-04-17 14:51:24 +0200

    use loops

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,22 @@
 #!/usr/bin/node
+const process = require('process');
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/people/18/';
-request(url, function (error, response, body) {
+const args = process.argv;
+const search = 'https://swapi-api.hbtn.io/api/people/18/';
+request(args[2], function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    console.log(JSON.parse(body).films.length);
+    let count = 0;
+    const chars = JSON.parse(body).results;
+    for (const i in chars) {
+      const result = chars[i].characters;
+      for (const j in result) {
+        if (result[j].includes(search)) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
   }
 });
