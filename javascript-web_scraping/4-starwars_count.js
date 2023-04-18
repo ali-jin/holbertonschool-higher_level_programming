cf662714104f2be1846be21a8b160b9df48acb1c
root <root@pc>
2023-04-18 11:58:56 +0200

    remove the else statment

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,21 +2,18 @@
 const process = require('process');
 const request = require('request');
 const args = process.argv;
-const search = 'https://swapi-api.hbtn.io/api/people/18/';
 request(args[2], function (error, response, body) {
   if (error) {
     console.log(error);
-  } else {
-    let count = 0;
-    const chars = JSON.parse(body).results;
-    for (const i in chars) {
-      const result = chars[i].characters;
-      for (const j in result) {
-        if (result[j].includes(search)) {
-          count++;
-        }
+  }
+  let count = 0;
+  const movies = JSON.parse(body).results;
+  for (const mov in movies) {
+    for (const char in mov.characters) {
+      if (char.includes('18')) {
+        count = count + 1;
       }
     }
-    console.log(count);
   }
+  console.log(count);
 });
